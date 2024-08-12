b1bb3d95c70c20a2d4eae9d2ac3f0e5a22eba194
Faizan Shabbir <faizanshabbir@gmail.com>
2024-08-12 15:57:08 -0500

    poc functionality test

--- api/index.py
+++ api/index.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api/videolink")
+def get_video_link():
+    return {"video_link": "https://www.youtube.com/watch?v=LFOx-vmYrts"}
+
+@app.get("/api/audiolink")
+def get_video_link():
+    return {"audio_link": "https://www.youtube.com/watch?v=LFOx-vmYrts"}
