41f7bde2a3f495a07ca0e078ed0a4e904024f319
Faizan Shabbir <faizanshabbir@gmail.com>
2024-08-12 17:06:37 -0500

    React player working with video full

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 import React from 'react'
-import ReactPlayer from 'react-player'
 import { useEffect, useState } from "react";
+import dynamic from 'next/dynamic'
 
 export default function Home() {
   const [video, setVideo] = useState("");
   const [audio, setAudio] = useState("");
   const [player, setPlayer] = useState(null);
+  const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
   const getVideo = async () => {
     console.log("fetching video");
@@ -14,7 +15,7 @@ export default function Home() {
     const data = await response.json();
     const videoLink = data.video_link;
     const embedLink = videoLink.replace("watch?v=", "embed/");
-    setVideo(embedLink);
+    setVideo(videoLink);
   };
 
   const getAudio = async () => {
@@ -31,11 +32,29 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex w-screen h-screen">
+    <main className="flex flex-col w-screen h-screen bg-black">
       {/* <div className="z-10 w-full max-w-5xl items-end justify-between font-mono text-sm lg:flex flex-col">
         <div><a href={video}>Video.</a></div>
         <div><a href={audio}>Audio.</a></div>
       </div> */}
+      <div className="relative w-screen h-screen">
+        <div className='absolute text-white'>
+          {/* <h1 className="text-xl text-right">Viber</h1> */}
+          <div><a href={video}>Video.</a></div>
+          <div><a href={audio}>Audio.</a></div>
+        </div>
+        <div className="flex w-screen h-screen cursor-non pointer-events-none">
+          <ReactPlayer
+            url={video}
+            width='100%'
+            height='100%'
+            controls= {false}
+            playing={true}
+            volume={0}
+            muted={true}
+          />
+        </div>
+      </div>
     </main>
   );
 }
