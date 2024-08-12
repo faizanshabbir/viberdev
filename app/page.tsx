972b5d8d3f6c97976631422bc37413351db580f2
Faizan Shabbir <faizanshabbir@gmail.com>
2024-08-12 16:27:57 -0500

    native YT player

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,15 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [video, setVideo] = useState("");
   const [audio, setAudio] = useState("");
+  const [player, setPlayer] = useState(null);
 
   const getVideo = async () => {
     console.log("fetching video");
     const response = await fetch("/api/videolink");
     const data = await response.json();
-    setVideo(data.video_link);
+    const videoLink = data.video_link;
+    const embedLink = videoLink.replace("watch?v=", "embed/");
+    setVideo(embedLink);
   };
 
   const getAudio = async () => {
@@ -19,17 +22,56 @@ export default function Home() {
     setAudio(data.audio_link);
   };
 
+  const onPlayerReady = (event: YT.PlayerEvent) => {
+    console.log("Player is ready");
+  };
+
+  const playVideo = () => {
+    if (player) {
+      player.playVideo();
+    }
+  };
 
   useEffect(() => { 
     getVideo() 
     getAudio()
+
+    // Load YouTube IFrame Player API script
+    const tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    if (firstScriptTag.parentNode) {
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
+    // Create YouTube player instance
+    window.onYouTubeIframeAPIReady = () => {
+      const newPlayer = new YT.Player('player', {
+        events: {
+          'onReady': onPlayerReady
+        }
+      });
+      setPlayer(newPlayer);
+    };
+
+    playVideo();
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-end justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-end justify-between font-mono text-sm lg:flex flex-col">
+    <main className="flex w-screen h-screen">
+      {/* <div className="z-10 w-full max-w-5xl items-end justify-between font-mono text-sm lg:flex flex-col">
         <div><a href={video}>Video.</a></div>
         <div><a href={audio}>Audio.</a></div>
+      </div> */}
+      <div className="w-screen h-screen flex-grow flex">
+        <iframe
+          width="100%"
+          height="100%"
+          src={`${video}?enablejsapi=1&controls=0`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="w-full h-full pointer-events-none"
+        ></iframe>
       </div>
     </main>
   );
