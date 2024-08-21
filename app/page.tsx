6db03715bbe51521a45afa716abdf57806fba92f
Faizan Shabbir <faizanshabbir@gmail.com>
2024-08-20 21:11:41 -0500

    video sliders, seeking and titles

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState, useRef, RefObject } from "react"
 import ReactPlayer from 'react-player'
 import { TbPlayerPlayFilled, TbPlayerPauseFilled, TbPlayerSkipForwardFilled } from "react-icons/tb";
 import { MdVolumeOff, MdVolumeUp } from "react-icons/md";
+import { VideoSeekSlider } from "react-video-seek-slider";
+import "react-video-seek-slider/styles.css"
 
 export default function Home() {
   const [video, setVideo] = useState("");
@@ -28,20 +30,27 @@ export default function Home() {
   const [initialPlay, setInitialPlay] = useState(true);
   const initialClick = () => setInitialPlay(false);
 
+  const [videoDuration, setVideoDuration] = useState(0);
+  const [audioDuration, setAudioDuration] = useState(0);
+  const setVideoDurationMilliseconds = (duration: number) => {
+    setVideoDuration(duration * 1000);
+  }
+  const setAudioDurationMilliseconds = (duration: number) => {
+    setAudioDuration(duration * 1000);
+  }
+
+  const [videoCurrentTime, setVideoCurrentTime] = useState(0);
+  const [audioCurrentTime, setAudioCurrentTime] = useState(0);
+
+  const [videoTitle, setVideoTitle] = useState("");
+  const [audioTitle, setAudioTitle] = useState("");
+
   const getVideo = async () => {
     console.log("fetching video");
     const response = await fetch("/api/videolink");
     const data = await response.json();
     const videoLink = data.video_link;
-    const embedLink = videoLink.replace("watch?v=", "embed/");
-    setVideo(videoLink);
-  };
-
-  const getVideoB = async () => {
-    console.log("fetching video");
-    const response = await fetch("/api/bvideolink");
-    const data = await response.json();
-    const videoLink = data.video_link;
+    getAndSetTitle(videoLink, true);
     const embedLink = videoLink.replace("watch?v=", "embed/");
     setVideo(videoLink);
   };
@@ -50,8 +59,20 @@ export default function Home() {
     console.log("fetching audio");
     const response = await fetch("/api/audiolink");
     const data = await response.json();
-    setAudio(data.audio_link);
+    const audioLink = data.audio_link;
+    getAndSetTitle(audioLink, false);
+    setAudio(audioLink);
   };
+
+  const getAndSetTitle = async (url: string, video: boolean) => {
+    const response = await fetch("http://noembed.com/embed?url=" + url);
+    const data = await response.json();
+    if(video){
+      setVideoTitle(data.title);
+    } else {
+      setAudioTitle(data.title);
+    }
+  }
 
   useEffect(() => { 
     getVideo() 
@@ -76,19 +97,32 @@ export default function Home() {
     }
   };
 
+  const playerSetSeek = (playerRef: RefObject<ReactPlayer>, time: number, video: boolean) => {
+    console.log("setting seek");
+    const timeSeconds = time / 1000;
+    console.log(timeSeconds);
+    console.log(playerRef.current);
+    if(playerRef.current){
+      playerRef.current.seekTo(timeSeconds);
+    }
+    if(video){
+      setVideoCurrentTime(time);
+      console.log({videoCurrentTime});
+    } else {
+      setAudioCurrentTime(time);
+      console.log({audioCurrentTime});
+    }
+  };
+
   // const togglePlaying = () => {
   //   setPlaying(!playing);
   // }
 
   return (
     <main className="flex flex-col w-screen h-screen bg-black">
-      {/* <div className="z-10 w-full max-w-5xl items-end justify-between font-mono text-sm lg:flex flex-col">
-        <div><a href={video}>Video.</a></div>
-        <div><a href={audio}>Audio.</a></div>
-      </div> */}
       <div className="relative w-screen h-screen">
-        <div className='absolute text-white'>
-          <div className='flex flex-row space-x-4 items-center'>
+        <div className='absolute text-white w-full bg-black pt-5 pb-4'>
+          <div className='flex flex-row space-x-4 min-h-[30px] items-center '>
             <a href={video}>Video.</a>
             {videoPlaying ? (
               <TbPlayerPauseFilled 
@@ -117,13 +151,23 @@ export default function Home() {
               />
             )}
             <TbPlayerSkipForwardFilled onClick={getVideo} style={{ cursor: 'pointer' }} title="Skip to next video" />
+            {!initialPlay && <div className="relative -translate-y-2 w-1/3" style={{ width: '30%' }}>
+              <VideoSeekSlider
+                max={videoDuration}
+                currentTime={videoCurrentTime}
+                bufferTime={400000}
+                onChange={((time) => playerSetSeek(videoPlayerRef, time, true))}
+                />
+            </div> }
+            <a href={video}>{videoTitle}</a>
+
           </div>
-          <div className='flex flex-row space-x-4 items-center'>
+          <div className='flex flex-row space-x-4 min-h-[30px] items-center'>
             <a href={audio}>Audio.</a>
             {audioPlaying ? (
               <TbPlayerPauseFilled 
                 onClick={audioPlayPause} 
-                style={{ cursor: 'pointer' }} 
+                style={{ cursor: 'pointer', }} 
                 title="Pause Video" 
               />
             ) : (
@@ -146,17 +190,26 @@ export default function Home() {
                 title="Play Video" 
               />
             )}
-            <TbPlayerSkipForwardFilled onClick={getAudio} style={{ cursor: 'pointer' }} title="Skip to next video" />
+            <TbPlayerSkipForwardFilled onClick={getAudio} style={{ cursor: 'pointer' }} title="Skip to next music" />
+            {!initialPlay && <div className="relative -translate-y-2 w-1/3" style={{ width: '30%' }}>
+              <VideoSeekSlider
+                max={audioDuration}
+                currentTime={audioCurrentTime}
+                bufferTime={400000}
+                onChange={((time) => playerSetSeek(audioPlayerRef, time, false))}
+                />
+            </div> }
+            <a href={audio}>{audioTitle}</a>
           </div>
         </div>
-        {initialPlay && <div className="absolute w-screen h-screen bg-black opacity-80 flex items-center justify-center text-white">
+        {initialPlay && <div className="absolute w-screen h-screen bg-black flex items-center justify-center text-white">
           <TbPlayerPlayFilled 
             onClick={() => {
               audioPlayPause();
               videoPlayPause();
               initialClick();
             }} 
-            style={{ cursor: 'pointer' }} 
+            style={{ cursor: 'pointer', fontSize: '100px'  }} 
             title="Play Vibes" 
           />
         </div>}
@@ -171,6 +224,7 @@ export default function Home() {
             volume={70}
             muted={videoMuted}
             onEnded={getVideo}
+            onDuration={(duration) => setVideoDurationMilliseconds(duration)}
           />
         </div> 
         <div className="hidden">
@@ -184,6 +238,7 @@ export default function Home() {
               volume={70}
               muted={audioMuted}
               onEnded={getAudio}
+              onDuration={(duration) => setAudioDurationMilliseconds(duration)}
             />
         </div>
       </div>
